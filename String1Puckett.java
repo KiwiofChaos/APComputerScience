d12a8cc31699cdad4b85d87978c66b4f4dae74fb
KiwiofChaos <kiwiofchaos@outlook.com>
2026-01-15 20:36:58 -0600

    Added String1Puckett.java

---
+++ String1Puckett.java
@@ -0,0 +1,26 @@
+public class String1Puckett
+{
+    public static void main(String[] args) {
+        String stringToFind = "apple";
+
+        String string1 = "hello, I am a silly little string!!!";
+        String string2 = "wow, that is an apple you have there";
+
+        System.out.println("The length of String 1 is " + string1.length());
+        System.out.println("The length of String 2 is " + string2.length());
+
+        if (string1.compareTo(string2) == 0) {
+            System.out.println("String one and String two are equal!!!");
+        }
+        else {
+            System.out.println("String one and String two are not equal!!!");
+        }
+
+        int stringToFindIndex = string2.indexOf(stringToFind);
+
+        System.out.println("The word '" + stringToFind + "' can be foud in String two at indecies " + stringToFindIndex + " to " + (stringToFindIndex + (stringToFind.length()) - 1) + ".");
+
+        System.out.println("The substring of String one from the fifth character onwards is '" + string1.substring(4) + "'.");
+        System.out.println("The substring of String two that goes from character five to character ten is '" + (string2.substring(4, 10) + "'."));
+    }
+}
