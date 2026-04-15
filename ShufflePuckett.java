95b487f45e014c666e4a6e3ad593790250a4bab4
KiwiofChaos <kiwiofchaos@outlook.com>
2026-04-15 09:24:49 -0500

    hjelp

---
+++ ShufflePuckett.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class ShufflePuckett {
+    public static void main(String[] args) {
+
+    }
+
+    public static ArrayList<Integer> shuffle(int[] someArray) {
+        ArrayList<Integer> someArrayList = new ArrayList<Integer>();
+        for (int someNumber: someArray) {
+            someArrayList.add(some_number);
+        }
+    }
+}
